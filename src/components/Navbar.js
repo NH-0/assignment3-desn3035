1e596e54e647ad6133502a26f164f58796a5886a
0NH <nj0214@hotmail.com>
2024-12-04 18:41:31 +0000

    setting up react and navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Logo from '../assets/Scentful Bliss.png'
+
+function Navbar() {
+  return (
+    <div className="navbar">
+    <div className="leftside"></div>
+    <img src={Logo} />
+    <div className="rightside">
+
+    </div>
+    </div>
+  )
+}
+
+export default Navbar
